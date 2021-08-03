38d5f363274da2e8ac9a84ac106d1cbd477f081f
zwd <812329088@qq.com>
2021-08-03 13:46:03 +0800

    first commit

---
+++ src/main/java/com/zwd/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zwd.git;
+
+/**
+ * @author zwd
+ * @create 2021-08-0313:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
