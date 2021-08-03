8b744dfc58d3c432638643dbd6102c59ba8103c2
zwd <812329088@qq.com>
2021-08-03 17:12:42 +0800

    push commit

--- src/main/java/com/zwd/git/GitTest.java
+++ src/main/java/com/zwd/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git5!");
         System.out.println("hello master!");
         System.out.println("hello git45!");
+        System.out.println("hello push!");
     }
 }
